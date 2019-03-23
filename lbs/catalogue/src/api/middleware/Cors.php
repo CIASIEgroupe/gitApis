f4e878ce2d19287582108ffc9cd2aa9fda02275a
lmaillard <33518682+lmaillard@users.noreply.github.com>
2019-03-23 19:06:50 +0100

    Update Cors.php

--- lbs/catalogue/src/api/middleware/Cors.php
+++ lbs/catalogue/src/api/middleware/Cors.php
@@ -12,7 +12,7 @@ class Cors{
 		$response = $next($request, $response);
 		$response = $response->withHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 			->withHeader("Access-Control-Allow-Headers", "X-Resquest-With, Content-Type, Accept, Origin, Authorization")
-			->withHeader("Access-Control-Allow-Origin", "*");
+			->withHeader("Access-Control-Allow-Origin", "*")
 			->withHeader("Origin", "http://api.catalogue.local");
 		return $response;
 	}
